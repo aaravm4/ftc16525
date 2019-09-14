2781144ce01322160009aa2c3aa25adbc0b997d2
aaravm4 <40776764+aaravm4@users.noreply.github.com>
2019-09-14 14:55:44 -0500

    Create BasicDriveOpMode.java
    This OpMode when ran, can let the driver drive their vehicle based on gamepad.

---
+++ BasicDriveOpMode.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="BasicDrive Op Mode", group="Linear Opmode")
+
+public class DriveOpMode extends LinearOpMode {
+    private DcMotor leftDrive;
+    private DcMotor rightDrive;
+
+    public void init() {
+        leftDrive = hardwareMap.get('leftMotor');
+        rightDrive = hardwareMap.get('rightMotor');
+        telemetry.addData("Status: ","Runnning");
+        telemetry.update();
+    }
+
+    public void runOpMode() {
+        while (opModeIsActive()) {
+            /* check gamestick val and config */
+            float left_power = -gamepad1.left_stick_y;
+            float right_power = -gamepad1.right_stick_y;
+            /* sets power based on gamepad */
+            leftDrive.setPower(left_power);
+            rightDrive.setPower(right_power);
+            telemetry.addData("Left Motor Power: ", left_power);
+            telemetry.addData("Right Motor Power: ", right_power);
+            telemetry.update();
+        }
+    }
+
+}
