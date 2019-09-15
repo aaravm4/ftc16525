9fc13dd9bd47e337c7c0ca21caa5959f318ffd92
GetRektOrElse <41167684+GetRektOrElse@users.noreply.github.com>
2019-09-15 16:25:22 -0500

    Update BasicDriveOpMode.java

--- BasicDriveOpMode.java
+++ BasicDriveOpMode.java
@@ -16,23 +16,17 @@ public class BasicDriveOpMode extends LinearOpMode {
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
 
-    @Override
-    public void init() {
-        /*When driver presses init*/
-        telemetry.addData("Status: ", "Getting Configurations");
-        telemetry.update();
-        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
-        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
-        telemetry.addData("Status: ", "Initialized");
-        telemtry.update();
-    }
 
     @Override
     public void runOpMode() {
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+        
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
+      
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -63,9 +57,7 @@ public class BasicDriveOpMode extends LinearOpMode {
             leftDrive.setPower(leftPower);
             rightDrive.setPower(rightPower);
 
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status: ", "Taking Gamepad Data ...")
-            telemetry.update();
+            // Show the elapsed game time and wheel p
         }
     }
 }
