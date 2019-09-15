1e2192e3ed2e665c475ab2521f5e5f6a9c5efb05
aaravm4 <40776764+aaravm4@users.noreply.github.com>
2019-09-15 16:18:31 -0500

    Update BasicDriveOpMode.java

--- BasicDriveOpMode.java
+++ BasicDriveOpMode.java
@@ -1,32 +1,71 @@
+
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
-        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-        import com.qualcomm.robotcore.hardware.DcMotor;
-        import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="BasicDrive Op Mode", group="Linear Opmode")
-
+@TeleOp(name="BasicDriveOpMode", group="Linear Opmode")
+@Disabled
 public class BasicDriveOpMode extends LinearOpMode {
-    private DcMotor leftDrive;
-    private DcMotor rightDrive;
 
+    // Declare OpMode members.
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+
+    @Override
+    public void init() {
+        /*When driver presses init*/
+        telemetry.addData("Status: ", "Getting Configurations");
+        telemetry.update();
+        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+        telemetry.addData("Status: ", "Initialized");
+        telemtry.update();
+    }
+
+    @Override
     public void runOpMode() {
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            leftDrive = hardwareMap.get(leftMotor);
-            rightDrive = hardwareMap.get(rightMotor);
-            telemetry.addData("Status: ","Runnning");
-            telemetry.update();
-            /* check gamestick val and config */
-            float left_power = -gamepad1.left_stick_y;
-            float right_power = -gamepad1.right_stick_y;
-            /* sets power based on gamepad */
-            leftDrive.setPower(left_power);
-            rightDrive.setPower(right_power);
-            telemetry.addData("Left Motor Power: ", left_power);
-            telemetry.addData("Right Motor Power: ", right_power);
+
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower;
+            double rightPower;
+
+            // Choose to drive using either Tank Mode, or POV Mode
+            // Comment out the method that's not used.  The default below is POV.
+
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+            double drive = -gamepad1.left_stick_y;
+            double turn  =  gamepad1.right_stick_x;
+            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+            // Tank Mode uses one stick to control each wheel.
+            // - This requires no math, but it is hard to drive forward slowly and keep straight.
+            // leftPower  = -gamepad1.left_stick_y ;
+            // rightPower = -gamepad1.right_stick_y ;
+
+            // Send calculated power to wheels
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status: ", "Taking Gamepad Data ...")
             telemetry.update();
         }
     }
-
 }
