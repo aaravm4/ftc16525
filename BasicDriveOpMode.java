43e8d57f36760d424c04684865404c3af48722b7
aaravm4 <40776764+aaravm4@users.noreply.github.com>
2019-09-17 20:36:05 -0500

    Update BasicDriveOpMode.java

--- BasicDriveOpMode.java
+++ BasicDriveOpMode.java
@@ -1,3 +1,10 @@
+/**
+ * This program is created by Aarav M and Avi A
+ * For the 2019 FTC Challenge
+ * The Team it was created for was Skyrise Team 15625
+ * Beginners are welcome to use this code
+ * To get bearings on BASIC Onbot Java
+ */
 
 
 package org.firstinspires.ftc.teamcode;
