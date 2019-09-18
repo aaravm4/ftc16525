e96020788badb20992618370d6dcfc93f4a04e6c
aaravm4 <40776764+aaravm4@users.noreply.github.com>
2019-09-17 20:34:49 -0500

    Update BasicDriveOpMode.java

--- BasicDriveOpMode.java
+++ BasicDriveOpMode.java
@@ -1,37 +1,52 @@
-/**
- * This program is created by Aarav M and Avi A
- * For the 2019 FTC Challenge
- * The Team it was created for was Skyrise Team 15625
- * Beginners are welcome to use this code
- * To get bearings on BASIC Onbot Java
- */
 
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Drive With Gamepads", group="Linear Opmode")
+
+@TeleOp(name="Basic: Linear OpMode Cool", group="Linear Opmode")
+
 public class BasicDriveOpMode extends LinearOpMode {
 
     // Declare OpMode members.
-    private DcMotor leftDrive;
-    private DcMotor rightDrive;
-
-
-    leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
-    rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
-    /* When you mount motors they usually run in different directions */
-    rightDrive.setDirection(DcMotor.Direction.REVERSE);
-
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
 
     @Override
     public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        leftDrive  = hardwareMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        
+        waitForStart();
+        runtime.reset();
+
         while (opModeIsActive()) {
-            leftDrive.setPower(-gamepade1.left_stick_y);
-            rightDrive.setPower(-gamepad1.right_sitck_y);
+
+            double leftPower;
+            double rightPower;
+
+            leftPower = -gamepad1.left_stick_y;
+            rightPower  =  -gamepad1.left_stick_y;
+            
+            leftDrive.setPower(leftPower/2);
+            rightDrive.setPower(rightPower/2);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.update();
         }
     }
 }
