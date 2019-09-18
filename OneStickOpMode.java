e67c9ed156f1fded52a9b96b8ae76bbab5fdc529
aaravm4 <40776764+aaravm4@users.noreply.github.com>
2019-09-17 20:36:40 -0500

    Create OneStickOpMode.java

---
+++ OneStickOpMode.java
@@ -0,0 +1,7 @@
+/**
+ * This program is created by Aarav M and Avi A
+ * For the 2019 FTC Challenge
+ * The Team it was created for was Skyrise Team 15625
+ * Beginners are welcome to use this code
+ * To get bearings on BASIC Onbot Java
+ */
