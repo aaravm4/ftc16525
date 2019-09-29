62429368cc5492e9c4a57c2a50720bc267d7abbd
aaravm4 <40776764+aaravm4@users.noreply.github.com>
2019-09-29 16:13:25 -0500

    Update BasicDriveOpMode.java

--- BasicDriveOpMode.java
+++ BasicDriveOpMode.java
@@ -1,31 +1,55 @@
-/**
- * This program is created by Aarav M and Avi A
- * For the 2019 FTC Challenge
- * The Team it was created for was Skyrise Team 15625
- * Beginners are welcome to use this code
- * To get bearings on BASIC Onbot Java
- */
- 
+/*
+Copyright 2019 FIRST Tech Challenge Team 16525
 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
-
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Remove a @Disabled the on the next line or two (if present) to add this opmode to the Driver Station OpMode list,
+ * or add a @Disabled annotation to prevent this OpMode from being added to the Driver Station
+ */
 @TeleOp(name="Drive Controlled", group="Linear Opmode")
 
-public class BasicDriveOpMode extends LinearOpMode throws InterruptedException {
+public class BasicDriveOpMode extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
     private DcMotor arm_motor = null;
+    private Servo grabber_turn = null;
+    private Servo grabber_vert = null;
     // private Servo bringPlatform = null;
 
     @Override
@@ -35,9 +59,9 @@ public class BasicDriveOpMode extends LinearOpMode throws InterruptedException {
 
         leftDrive  = hardwareMap.get(DcMotor.class, "leftDrive");
         rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
-        arm_motor = hardwareMap.get(DcMotor.class, "arm_motor")
-        bringPlatform = hardwareMap.get(Servo.class, "bringPlat")
-            
+        arm_motor = hardwareMap.get(DcMotor.class, "arm_motor");
+        grabber_turn = hardwareMap.get(Servo.class, "grabber_turn");
+        grabber_vert = hardwareMap.get(Servo.class, "grabber_vert");
             
         telemetry.addData("Status: ", "Hardware Configured");
         telemetry.update();
@@ -52,28 +76,36 @@ public class BasicDriveOpMode extends LinearOpMode throws InterruptedException {
 
             double leftPower;
             double rightPower;
-            double armPower
+            double armPower;
+            double turnPower;
+            double upPower;
+            double bumpPower;
 
             leftPower = -gamepad1.left_stick_y;
             rightPower  =  -gamepad1.right_stick_y;
             armPower = -gamepad2.left_stick_y;
+            turnPower = -gamepad2.right_stick_x;
+            bumpPower = 0;
             
+
             leftDrive.setPower(leftPower/2);
             rightDrive.setPower(rightPower/2);
-            arm_motor.setPosition(armPower/2);
+            arm_motor.setPower(armPower/2);
+            grabber_turn.setPosition(turnPower/2);
+            
+            if (gamepad2.right_bumper) {
+                grabber_vert.setPosition(bumpPower+.5);
+            } else {
+                grabber_vert.setPosition(bumpPower-.5);
+            }
             
             // if (gamepad1.left_bumper) {
-            // 		bringPlatform.setPosition(1);
-          	// }
-          	// else if (gamepad1.right_bumper) {
-            //		bringPlatform.setPosition(0);
-          	// }
+            //         bringPlatform.setPosition(1);
+              // }
+              // else if (gamepad1.right_bumper) {
+            //        bringPlatform.setPosition(0);
+              // }
 
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Left Motor: ", leftPower.toString());
-            telemetry.addData("Right Motor: ", rightPower.toString());
-            telemetry.addData("Arm Motor: ", armPower.toString());
-            telemetry.update();
         }
     }
 }
