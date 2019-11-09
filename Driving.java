aa9b2d02cc87db589cc625a0854e9adc9fa8daba
aaravm4 <40776764+aaravm4@users.noreply.github.com>
2019-11-08 19:11:36 -0600

    Update Driving.java

--- Driving.java
+++ Driving.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.skystone;
 
 import org.firstinspires.ftc.teamcode.common.RobotConfig;
+import com.qualcomm.robotcore.util.Hardware;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Blinker;
 import com.qualcomm.robotcore.hardware.Gyroscope;
@@ -13,8 +14,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Driving extends LinearOpMode {
 
+    
     // Declare OpMode members.
     private Servo bringPlatform = null;
+    
+    // Declaring Variables
+    private double leftPower;
+    private double rightPower;
+    private double armPower;
+    private double turnPower;
+    private double upPower;
+    private double bumpPower;
+    private double platPower;
     
     RobotConfig robotConfig = new RobotConfig(this);
     
@@ -27,9 +38,6 @@ public class Driving extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-
-
-
         telemetry.addData("Status: ", "Hardware Configured");
         telemetry.update();
 
@@ -41,44 +49,38 @@ public class Driving extends LinearOpMode {
 
         while (opModeIsActive()) {
 
-            double leftPower;
-            double rightPower;
-            double armPower;
-            double turnPower;
-            double upPower;
-            double bumpPower;
-            double platPower;
-
+           
 
             leftPower = gamepad1.left_stick_y;
             rightPower  =  gamepad1.right_stick_y;
-            armPower = -gamepad2.left_stick_y;
+            armPower = gamepad2.left_stick_y;
             turnPower = -gamepad2.right_stick_x;
             bumpPower = 0;
 
 
-
+           
             robotConfig.leftDrive.setPower(leftPower/2);
             robotConfig.rightDrive.setPower(rightPower/2);
             robotConfig.arm_motor.setPower(armPower/3);
-            robotConfig.grabber_turn.setPosition(turnPower/2);
-            double secondturn_pow = 0;
+            robotConfig.grabber_turn_two.setPosition(turnPower/2);
+
 
             if (gamepad2.right_bumper) {
                 robotConfig.grabber_vert.setPosition(bumpPower+.5);
             } else if (gamepad2.left_bumper){
                 robotConfig.grabber_vert.setPosition(bumpPower-.5);
             } else if (gamepad1.left_bumper){
-                robotConfig.bringPlatform.setPosition(1);
+                robotConfig.bringPlatform.setPosition(.9);
             } else if (gamepad1.right_bumper){
-                robotConfig.bringPlatform.setPosition(0);
-            } else if (gamepad2.x){  
-                  secondturn_pow += .2;
-                robotConfig.grabber_turn_two.setPosition(secondturn_pow);
-            } else if (gamepad2.b){  
-                  secondturn_pow -= .2;
-                robotConfig.grabber_turn_two.setPosition(secondturn_pow);
+                robotConfig.bringPlatform.setPosition(0); 
             }
+            if (gamepad2.x){
+                robotConfig.grabber_turn.setPosition(turnPower+.4);
+            }
+            if (gamepad2.b){
+                robotConfig.grabber_turn.setPosition(turnPower-.4);
+            } 
+          
 
         }
     }
